0d5578e3798bf3f90fdd54c76796f40c93bbd9d9
Alfie-1516 <okorochaalfred@gmail.com>
2024-07-28 10:58:48 -0500

    Added in the check for the userinput,and the call to the block draw function from the block class. Added in the initialization function to the position class

--- Position_Class.py
+++ Position_Class.py
@@ -1,1 +1,6 @@
 """The Position class represents a coordinate on a grid with row and column attributes."""
+
+class Position:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
