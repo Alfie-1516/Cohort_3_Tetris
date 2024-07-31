8da59d5e8f2d5454c3dce82a9eb902eb4935c9f3
CourtWri <courtwright20@gmail.com>
2024-07-30 21:17:02 -0400

    fixed L block

--- Diff_Blocks.py
+++ Diff_Blocks.py
@@ -27,10 +27,10 @@ class LBlock(Block):
   def __init__(self):
       super().__init__(id = 3)
       self.cells = {
-        0: [Position(0, 0), Position (0, 1), Position (1, 0), Position(1, 1)],
-        1: [Position(1, 1), Position (0, 0), Position (0, 1), Position(1, 0)],
-        2: [Position(0, 0), Position (0, 1), Position (1, 0), Position(0, 1)],
-        3: [Position(0, 0), Position (0, 1), Position (1, 0), Position(1, 1)]
+        0: [Position(0, 2), Position (1, 0), Position (1, 1), Position(1, 2)],
+        1: [Position(0, 1), Position (1, 1), Position (2, 1), Position(2, 2)],
+        2: [Position(1, 0), Position (1, 1), Position (1, 2), Position(2, 0)],
+        3: [Position(0, 0), Position (0, 1), Position (1, 1), Position(2, 1)]
       }
 class OBlock(Block):
   def __init__(self):
