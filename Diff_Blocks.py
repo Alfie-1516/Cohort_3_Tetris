dc0890d5d1d9bbd4fc683c9d28b6e80dd388609c
CourtWri <courtwright20@gmail.com>
2024-08-05 20:15:22 -0400

    block id

--- Diff_Blocks.py
+++ Diff_Blocks.py
@@ -7,7 +7,7 @@ from Block_Class import Block
 
 class IBlock(Block):
   def __init__(self):
-    super().__init__(id = 1)
+    super().__init__(id = 3)
     self.cells = {
       0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
       1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
@@ -28,7 +28,7 @@ class JBlock(Block):
 
 class LBlock(Block):
   def __init__(self):
-    super().__init__(id = 3)
+    super().__init__(id = 1)
     self.cells = {
       0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
       1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
